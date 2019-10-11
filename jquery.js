408cafb6fd02c0f6e495cebf418441becfb8cf92
Manuel <sufferdarkness44@gmmail.com>
2019-10-10 20:03:43 -0500

    version1

---
+++ jquery.js
@@ -0,0 +1,61 @@
+$(document).ready(function(){
+
+ $(".Cruz").click(function() {
+
+	 $("div[type]").css({'background':'yellow'});
+	  $("#seis,#uno,#siete,#ocho,#once,#veintiseis,#veintinueve,#veinticinco,#treintaiuno,#treintaicinco,#dos,#cinco,#doce,#treinta,#treintaiseis,#treintaidos").css({'background':'white'});
+
+   });
+ 
+  $(".RayDer").click(function() {
+	 $("div[style], #dieciocho, #veinticuatro").css({'background-color':'red','background':'yellow'});
+	 $("#uno,#treintaidos,#siete,#dos,#cinco,#ocho,#once,#veintiseis,#veintinueve,#veinticinco,#treintaiuno,#tres,#cuatro,#nueve,#diez,#trece,#catorce,#quince,#dieciseis,#diecisiete,#diecinueve,#veinte,#veintiuno,#veintidos,#veintitres,#veintisiete,#veintiocho,#treintaitres,#treintaicuatro,#treintaicinco").css({'background':'white'});
+
+   });
+
+    $(".RayIzq").click(function() {
+
+ 
+	 $("div[mode],#trece,#diesisiete").css({'background-color':'red','background':'yellow'});
+	 $("#tres,#cuatro,#nueve,#diez,#catorce,#quince,#dieciseis,#diecisiete,#dieciocho,#veinte,#veintiuno,#veintidos,#veintitres,#veinticuatro,#veintisiete,#veintiocho,#treintaitres,#treintaicuatro,#seis,#doce,#treinta,#treintaiseis,#dos,#cinco,#treintaidos,#treintaicinco,#ocho,#once,#veintiseis,#veintinueve").css({'background':'white'});
+	
+
+   });
+
+        $(".Arriba").click(function() {
+ 	  $("#uno,#dos,#tres,#cuatro,#cinco,#seis").css({'background':'yellow'});
+	 $("#ocho,#once,#veintiseis,#veintinueve,#nueve,#diez,#doce,#trece,#catorce,#quince,#dieciseis,#diecisiete,#dieciocho,#diecinueve,#veinte,#veintiuno,#veintidos,#veintitres,#veinticuatro,#veintisiete,#veintiocho,#siete,#treinta,#treintaitres,#treintaicuatro,#treintaidos,#treintaicinco,#veinticinco,#treintaiuno,#treintaiseis").css({'background':'white'});
+	
+	
+
+   });
+
+   
+        $(".Abajo").click(function() {
+
+	  $("#veintiseis,#veintinueve,#ocho,#once,#uno,#dos,#tres,#cuatro,#cinco,#seis,#siete,#treinta,#nueve,#diez,#doce,#trece,#catorce,#quince,#dieciseis,#diecisiete,#diecinueve,#veinte,#veintiuno,#veintidos,#veintitres,#veinticuatro,#veinticinco,#dieciocho,#veintisiete,#veintiocho").css({'background':'white'});
+	  $("#treintaiuno,#treintaidos,#treintaitres,#treintaicuatro,#treintaicinco,#treintaiseis").css({'background':'yellow'});
+	
+	
+
+   });
+
+        $(".Equis").click(function() {
+
+	  $("#treintaidos,#treintaitres,#treintaicuatro,#treintaicinco,#uno,#dos,#tres,#cuatro,#cinco,#seis,#siete,#treinta,#nueve,#diez,#doce,#trece,#catorce,#quince,#dieciseis,#diecisiete,#diecinueve,#veinte,#veintiuno,#veintidos,#veintitres,#veinticuatro,#veinticinco,#dieciocho,#veintisiete,#veintiocho").css({'background':'white'});
+	  $("#uno,#seis,#ocho,#once,#quince,#dieciseis,#veintiuno,#veintidos,#veintiseis,#veintinueve,#treintaiuno,#treintaiseis").css({'background':'yellow'});
+	
+	
+
+   });
+     
+       $(".FigRar").click(function() {
+
+	  $("#ocho,#veintiseis,#veintinueve,#quince,#dieciseis,#veintiuno,#veintidos,#once,#uno,#dos,#tres,#cuatro,#cinco,#seis,#treintaiuno,#treintaidos,#treintaitres,#treintaicuatro,#treintaicinco,#treintaiseis,div[mode],#trece,#diesisiete,#doce,#dieciocho,#veinticuatro,#treinta").css({'background':'yellow'});
+	  $("#nueve,#diez,#diecisiete,#veintitres,#veintisiete,#veintiocho,#catorce,#veinte").css({'background':'white'});
+	
+	
+
+   });
+ 
+});
